49ef3aeda36fe1006366ff52a443337bc20f3640
David Edey <david+github@radixdlt.com>
2021-11-04 17:46:08 +0000

    Add open api client generation

---
+++ generation/README.md
@@ -0,0 +1,32 @@
+# Generated Dependencies
+
+Details on each generated dependency are below.
+
+## RadixNodeApi.GeneratedApiClient
+
+This is generated from the node's transactions api spec.
+
+Prerequisites:
+* Install the [openapi-generator](https://github.com/OpenAPITools/openapi-generator) (eg with `brew install openapi-generator`)
+* Installed the dotnet SDK version 5+
+
+With all paths relative to the repo root, proceed as follows:
+
+* Update `./generation/transactions-api-spec.json` with the latest spec
+* Update the target version in `./generation/regenerate-transactions-client.sh`
+* Run `./generation/regenerate-transactions-client.sh`
+* Copy the new version from `./generation/packed` to `./generated`
+* Update the version in `./Directory.Build.props`
+
+### Docs
+* Docs on [general openapi-generator configuration are here](https://openapi-generator.tech/docs/configuration/).
+* Docs on the [csharp-netcore specific configuration are here](https://openapi-generator.tech/docs/generators/csharp-netcore).
+* The PR implementing the [httpclient implementation is here](https://github.com/OpenAPITools/openapi-generator/pull/8821).
+
+### Transitive dependencies
+
+For reference, at initial build time, the dependencies specified were:
+  * "JsonSubTypes": "1.8.0",
+  * "Newtonsoft.Json": "12.0.3",
+  * "Polly": "7.2.1", (https://github.com/App-vNext/Polly - for retry logic)
+  * "System.ComponentModel.Annotations": "5.0.0"
---
+++ generation/regenerate-transactions-client.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd "$SCRIPT_DIR"
+
+#############
+# VARIABLES #
+#############
+
+packageVersion='0.2.0' # This needs bumping every time
+
+##########
+# CHECKS #
+##########
+
+if [ -d packed ]; then
+  existsCheck=`ls ./packed | grep $packageVersion`
+else
+  existsCheck=''
+fi
+
+if [ ! -z "$existsCheck" ]; then
+    echo "Package with version $packageVersion has already been built."
+    echo "You should bump the version so that NuGeT doesn't cache the previous version."
+    echo "If you wish to ignore this error, just delete the old package from the ./packed folder."
+    exit 1
+fi
+
+#########
+# REGEN #
+#########
+
+# Find dummy diectory
+for TMPDIR in "$TMPDIR" "$TMP" /var/tmp /tmp
+do
+    test -d "$TMPDIR" && break
+done
+
+dummyApiDirectory="$TMPDIR/radix-generated-api/"
+
+rm -rf "$dummyApiDirectory"
+mkdir "$dummyApiDirectory"
+
+openapi-generator generate \
+    -i ./transactions-api-spec.json \
+    -g csharp-netcore \
+    -o "$dummyApiDirectory" \
+    --library httpclient \
+    --additional-properties=packageName=RadixNodeApi.GeneratedApiClient,targetFramework=net5.0,packageVersion=$packageVersion
+
+cd "$dummyApiDirectory"
+dotnet pack
+
+cd "$SCRIPT_DIR"
+mkdir -p ./packed
+find "$dummyApiDirectory/src/RadixNodeApi.GeneratedApiClient/bin/Debug" -name \*.nupkg -exec cp {} ./packed \;
+
+# Clear up generated api so that it doesn't interfere with indexes/lookups
+rm -rf "$dummyApiDirectory"
