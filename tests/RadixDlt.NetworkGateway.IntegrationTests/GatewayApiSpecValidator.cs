8dadda137182fcb56bba206627cbbd16a71b3901
Alex Morgulis <alexander.morgulis@rdx.works>
2022-08-12 19:10:44 -0400

    added request parameters validation

--- tests/RadixDlt.NetworkGateway.IntegrationTests/GatewayApiSpecValidator.cs
+++ tests/RadixDlt.NetworkGateway.IntegrationTests/GatewayApiSpecValidator.cs
@@ -7,7 +7,7 @@ namespace RadixDlt.NetworkGateway.IntegrationTests
 {
     internal static class GatewayApiSpecValidator
     {
-        private static readonly string _openApispecFileName = "../../../../../gateway-api-spec.yaml";
+        private static readonly string _openApiFileName = "../../../../../gateway-api-spec.yaml";
 
         private static OpenApiDocument? _openApiDocument = null;
 
@@ -15,7 +15,7 @@ namespace RadixDlt.NetworkGateway.IntegrationTests
         {
             if (_openApiDocument == null)
             {
-                string stringYml = File.ReadAllText(_openApispecFileName);
+                string stringYml = File.ReadAllText(_openApiFileName);
 
                 // Read V3 as YAML
                 _openApiDocument = new OpenApiStringReader().Read(stringYml, out var diagnostic);
@@ -31,40 +31,55 @@ namespace RadixDlt.NetworkGateway.IntegrationTests
                                                         ca.AttributeType.Name == "HtttpPutAttribute" ||
                                                         ca.AttributeType.Name == "HttpDeleteAttribute") != null);
 
-            // extract /transaction endpoints from the specification
-            var apiPathItems = _openApiDocument.Paths.Where(p => p.Key.Contains(controllerRoute));
+            // extract controller endpoints from the openApi specification
+            var openApiPathItems = _openApiDocument.Paths.Where(p => p.Key.Contains(controllerRoute));
 
-            // verify that the number of endpoints matches (first parameter is 'expected', second is 'actual'
+            // verify that the number of endpoints matches (first parameter is 'expected', second is 'actual')
             Assert.True(
-                apiPathItems.Count() == methodInfos.Count(),
-                $"The number of endpoints in spec ({apiPathItems.Count()}) does not match the number of implemented endpoints ({methodInfos.Count()}).");
+                openApiPathItems.Count() == methodInfos.Count(),
+                $"The number of endpoints in openApi ({openApiPathItems.Count()}) does not match the number of implemented endpoints ({methodInfos.Count()}).");
 
-            // loop through the spec
-            foreach (var specApiItem in apiPathItems)
+            // loop through openApi items
+            foreach (var openApiPathItem in openApiPathItems)
             {
-                var specUrl = specApiItem.Key;
+                // main route
+                var openApiItemUrl = openApiPathItem.Key;
 
-                foreach (var op in specApiItem.Value.Operations)
+                // loop through CRUD ops
+                foreach (var op in openApiPathItem.Value.Operations)
                 {
-                    var specOpCode = op.Key.ToString();
-                    string specResponseParameter = specApiItem.Value.Operations[op.Key].Responses["200"].Content["application/json"].Schema.Reference.Id;
-
-                    // check if the endpont exists url + opCode
+                    // GET/POST/PUT/DELETE
+                    var openApiOpCode = op.Key.ToString();
+                   
+                    // check if the route exists in the code
                     var endpoint = methodInfos.Where(m => m.CustomAttributes
                         .FirstOrDefault(ca =>
-                                ca.AttributeType.Name == $"Http{specOpCode}Attribute" &&
+                                ca.AttributeType.Name == $"Http{openApiOpCode}Attribute" &&
                                 ca.ConstructorArguments[0].ArgumentType == typeof(string) &&
-                                specUrl.EndsWith(ca.ConstructorArguments[0].Value.ToString())) != null).ToList();
+                                openApiItemUrl.EndsWith(ca.ConstructorArguments[0].Value.ToString())) != null).ToList();
 
-                    // Assert.True(endpoint.Count() == 1, $"The endpoint '{specUrl}' is not implemented.");
+                    // Assert.True(endpoint.Count() == 1, $"The endpoint '{openApiUrl}' is not implemented.");
 
-                    // verify the return type
                     if (endpoint.Any())
                     {
+                        // verify the response type
+                        string openApiResponseParameter = openApiPathItem.Value.Operations[op.Key].Responses["200"].Content["application/json"].Schema.Reference.Id;
+
                         string codeResponseParameter = endpoint[0].ReturnType.GenericTypeArguments[0].Name;
                         Assert.True(
-                            specResponseParameter.Equals(codeResponseParameter),
-                            $"Error in '{specUrl}'. The return parameter '{specResponseParameter}' is not equal to '{codeResponseParameter}'.");
+                            openApiResponseParameter.Equals(codeResponseParameter),
+                            $"Error in '{openApiItemUrl}'. The return parameter '{openApiResponseParameter}' is not equal to '{codeResponseParameter}'.");
+
+                        // verify request parameters
+                        var codeRequestParameters = endpoint[0].GetParameters().Select(p => p.ParameterType.Name);
+
+                        // schema request parameters
+                        var openApiRequestParameters = openApiPathItem.Value.Operations[op.Key].RequestBody.Content.Select(c => c.Value.Schema.Reference.Id);
+
+                        // compare with the schema request parameters including the order
+                        Assert.True(
+                            Enumerable.SequenceEqual(openApiRequestParameters, codeRequestParameters),
+                            $"Error in '{openApiItemUrl}'. The request parameters don't match.");
                     }
                 }
             }
