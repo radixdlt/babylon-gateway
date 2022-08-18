0e5fd55239bfa68853ff620ddc1aff96e019602d
Krzysztof Łabuś <krzysztof.labus@rdx.works>
2022-08-18 12:20:52 +0200

    Fixed missing service registrations

--- src/RadixDlt.NetworkGateway.PrometheusIntegration.DataAggregator/DataAggregatorBuilderExtensions.cs
+++ src/RadixDlt.NetworkGateway.PrometheusIntegration.DataAggregator/DataAggregatorBuilderExtensions.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Prometheus;
 using RadixDlt.NetworkGateway.DataAggregator;
+using RadixDlt.NetworkGateway.DataAggregator.Monitoring;
+using RadixDlt.NetworkGateway.DataAggregator.NodeServices;
+using RadixDlt.NetworkGateway.DataAggregator.NodeServices.ApiReaders;
+using RadixDlt.NetworkGateway.DataAggregator.Services;
 using RadixDlt.NetworkGateway.DataAggregator.Workers.GlobalWorkers;
 using RadixDlt.NetworkGateway.DataAggregator.Workers.NodeWorkers;
 
@@ -13,7 +17,21 @@ public static class DataAggregatorBuilderExtensions
         builder.Services
             .AddSingleton<MetricsObserver>()
             .AddSingleton<IGlobalWorkerObserver>(provider => provider.GetRequiredService<MetricsObserver>())
-            .AddSingleton<INodeWorkerObserver>(provider => provider.GetRequiredService<MetricsObserver>());
+            .AddSingleton<INodeWorkerObserver>(provider => provider.GetRequiredService<MetricsObserver>())
+            .AddSingleton<ILedgerConfirmationServiceObserver>(provider => provider.GetRequiredService<MetricsObserver>())
+            .AddSingleton<IMempoolPrunerServiceObserver>(provider => provider.GetRequiredService<MetricsObserver>())
+            .AddSingleton<IMempoolResubmissionServiceObserver>(provider => provider.GetRequiredService<MetricsObserver>())
+            .AddSingleton<IAggregatorHealthCheckObserver>(provider => provider.GetRequiredService<MetricsObserver>())
+            .AddSingleton<ISystemStatusServiceObserver>(provider => provider.GetRequiredService<MetricsObserver>())
+            .AddSingleton<INodeInitializerObserver>(provider => provider.GetRequiredService<MetricsObserver>())
+            .AddSingleton<IMempoolTrackerServiceObserver>(provider => provider.GetRequiredService<MetricsObserver>())
+            .AddSingleton<INodeTransactionLogWorkerObserver>(provider => provider.GetRequiredService<MetricsObserver>())
+            .AddSingleton<INodeMempoolTransactionIdsReaderWorkerObserver>(provider => provider.GetRequiredService<MetricsObserver>())
+            .AddSingleton<INodeMempoolFullTransactionReaderWorkerObserver>(provider => provider.GetRequiredService<MetricsObserver>())
+            .AddSingleton<IRawTransactionWriterObserver>(provider => provider.GetRequiredService<MetricsObserver>())
+            .AddSingleton<INetworkConfigurationReaderObserver>(provider => provider.GetRequiredService<MetricsObserver>())
+            .AddSingleton<INetworkStatusReaderObserver>(provider => provider.GetRequiredService<MetricsObserver>())
+            .AddSingleton<ITransactionLogReaderObserver>(provider => provider.GetRequiredService<MetricsObserver>());
 
         builder.CoreApiHttpClientBuilder
             .UseHttpClientMetrics();
