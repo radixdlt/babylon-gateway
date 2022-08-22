08cd2e52413d048d71ced376c75fb50ac29319f7
Alex Morgulis <alexander.morgulis@rdx.works>
2022-08-22 07:54:40 -0400

    PR/warnings fixes #1

--- tests/RadixDlt.NetworkGateway.IntegrationTests/GatewayApi/TestApplicationFactory.cs
+++ tests/RadixDlt.NetworkGateway.IntegrationTests/GatewayApi/TestApplicationFactory.cs
@@ -1,7 +1,6 @@
 using GatewayApi;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -10,7 +9,7 @@ namespace RadixDlt.NetworkGateway.IntegrationTests.GatewayApi
 {
     public class TestApplicationFactory : WebApplicationFactory<GatewayApiStartup>
     {
-        public const string NetworkName = "localnet";
+        public static readonly string NetworkName = "localnet";
 
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
@@ -18,19 +17,34 @@ namespace RadixDlt.NetworkGateway.IntegrationTests.GatewayApi
             {
                 var sp = services.BuildServiceProvider();
 
-                using var scope = sp.CreateScope();
-                var scopedServices = scope.ServiceProvider;
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
 
-                var logger = scopedServices
-                    .GetRequiredService<ILogger<TestApplicationFactory>>();
+                    var logger = scopedServices
+                        .GetRequiredService<ILogger<TestApplicationFactory>>();
+                }
             })
             .ConfigureAppConfiguration((hostingContext, config) =>
             {
                 config.AddEnvironmentVariables();
-                config.AddJsonFile("appsettings.Development.json", false, true);
+                // config.AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", false, true);
+                config.AddJsonFile($"appsettings.development.json", false, true);
             });
 
-            // builder.UseSolutionRelativeContentRoot("tests");
+            // builder.UseContentRoot("C:\\Work\\babylon-gateway\\samples\\GatewayApi");
+        }
+
+        protected override IWebHostBuilder CreateWebHostBuilder()
+        {
+            var hostBuilder = new WebHostBuilder()
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config.AddEnvironmentVariables();
+            })
+            .UseStartup<GatewayApiStartup>();
+
+            return hostBuilder;
         }
     }
 }
