2bfc6ddfa1e8979dea7f1cbb068583829bbdb08a
Krzysztof Łabuś <krzysztof.labus@rdx.works>
2022-08-25 10:54:35 +0200

    Internalized PrometheusIntegration libraries

--- src/RadixDlt.NetworkGateway.PrometheusIntegration/assembly.cs
+++ src/RadixDlt.NetworkGateway.PrometheusIntegration/assembly.cs
@@ -62,38 +62,7 @@
  * permissions under this License.
  */
 
-using Prometheus;
-using System;
+using System.Runtime.CompilerServices;
 
-namespace RadixDlt.NetworkGateway.PrometheusIntegration;
-
-internal enum MetricStatus
-{
-    Yes,
-    Unknown,
-    No,
-}
-
-internal static class MetricExtensions
-{
-    public static void SetStatus(this IGauge metric, MetricStatus status)
-    {
-        metric.Set(GetMetricValueForStatus(status));
-    }
-
-    public static void SetStatus(this IGauge metric, bool status)
-    {
-        metric.Set(GetMetricValueForStatus(status ? MetricStatus.Yes : MetricStatus.No));
-    }
-
-    private static double GetMetricValueForStatus(MetricStatus status)
-    {
-        return status switch
-        {
-            MetricStatus.Yes => 1,
-            MetricStatus.Unknown => 0.5,
-            MetricStatus.No => 0,
-            _ => throw new ArgumentOutOfRangeException(nameof(status), status, "Unknown metric status value"),
-        };
-    }
-}
+[assembly: InternalsVisibleTo("RadixDlt.NetworkGateway.PrometheusIntegration.DataAggregator")]
+[assembly: InternalsVisibleTo("RadixDlt.NetworkGateway.PrometheusIntegration.GatewayApi")]
