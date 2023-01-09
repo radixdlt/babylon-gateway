7ce627020eb59ca006cd711a9ca6b49f5d4ffc31
Krzysztof Łabuś <krzysztof.labus@rdx.works>
2023-01-09 14:55:45 +0100

    Another attempt to fix Npgsql enum conversion exception

--- src/RadixDlt.NetworkGateway.PostgresIntegration/DataAggregatorBuilderExtensions.cs
+++ src/RadixDlt.NetworkGateway.PostgresIntegration/DataAggregatorBuilderExtensions.cs
@@ -63,7 +63,6 @@
  */
 
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using RadixDlt.NetworkGateway.DataAggregator;
 using RadixDlt.NetworkGateway.DataAggregator.Services;
@@ -95,13 +94,11 @@ public static class DataAggregatorBuilderExtensions
             .AddSingleton<IPendingTransactionResubmissionService, PendingTransactionResubmissionService>()
             .AddSingleton<IPendingTransactionPrunerService, PendingTransactionPrunerService>();
 
-        // Useful links:
-        // https://www.npgsql.org/efcore/index.html
-        // https://www.npgsql.org/doc/connection-string-parameters.html
         builder.Services
+            .AddTypedNpgsqlDataSource<ReadWriteDbContext>(PostgresIntegrationConstants.Configuration.ReadWriteConnectionStringName)
             .AddDbContextFactory<ReadWriteDbContext>((serviceProvider, options) =>
             {
-                options.UseNpgsql(serviceProvider.GetRequiredService<IConfiguration>().GetConnectionString(PostgresIntegrationConstants.Configuration.ReadWriteConnectionStringName));
+                options.UseNpgsql(serviceProvider.GetRequiredService<NpgsqlDataSourceHolder<ReadWriteDbContext>>().NpgsqlDataSource);
             });
 
         return builder;
