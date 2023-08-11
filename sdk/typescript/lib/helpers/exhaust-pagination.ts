9960b9244505e9a1aa674e0bf7621f8d46686306
abstractfruitfactory <abstractfruitfactory@gmail.com>
2023-08-11 13:48:09 +0100

    feat: add methods with ledger state

--- sdk/typescript/lib/helpers/exhaust-pagination.ts
+++ sdk/typescript/lib/helpers/exhaust-pagination.ts
@@ -1,3 +1,5 @@
+import { LedgerState } from '../generated'
+
 /**
  * Exhausts a paginated API resource and returns all the results.
  */
@@ -20,4 +22,39 @@ export const exhaustPagination = async <T>(
   } while (next_cursor)
 
   return aggregatedEntities
+}
+
+/**
+ * Exhausts a paginated API resource and returns all the results.
+ */
+export const exhaustPaginationWithLedgerState = async <T>(
+  queryFunction: (
+    cursor?: string
+  ) => Promise<{
+    ledger_state: LedgerState
+    items: T[]
+    next_cursor?: string | null
+  }>,
+  start?: string
+) => {
+  let next_cursor: string | null | undefined = start
+  const aggregatedEntities: T[] = []
+
+  let ledgerState: LedgerState
+
+  do {
+    const queryFunctionResponse: {
+      next_cursor?: string | null
+      items: T[]
+      ledger_state: LedgerState
+    } = await queryFunction(next_cursor)
+    ledgerState = queryFunctionResponse.ledger_state
+    aggregatedEntities.push(...queryFunctionResponse.items)
+    next_cursor = queryFunctionResponse.next_cursor
+  } while (next_cursor)
+
+  return {
+    aggregatedEntities,
+    ledger_state: ledgerState,
+  }
 }
