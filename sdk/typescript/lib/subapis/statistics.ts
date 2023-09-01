dacc4af519bbfb8fe85d8b53d8455e49191923ac
abstractfruitfactory <abstractfruitfactory@gmail.com>
2023-09-01 09:23:51 +0100

    feat: add uptime params

--- sdk/typescript/lib/subapis/statistics.ts
+++ sdk/typescript/lib/subapis/statistics.ts
@@ -1,12 +1,14 @@
-import { StatisticsApi, ValidatorsUptimeResponse } from '../generated'
+import { LedgerStateSelector, StatisticsApi, ValidatorsUptimeResponse } from '../generated'
 
 export class Statistics {
-  constructor(public innerClient: StatisticsApi) {}
+  constructor(public innerClient: StatisticsApi) { }
 
-  getValidatorsUptime(validator_addresses: string[]): Promise<ValidatorsUptimeResponse> {
+  getValidatorsUptime(validator_addresses: string[], from_ledger_state?: LedgerStateSelector, at_ledger_state?: LedgerStateSelector): Promise<ValidatorsUptimeResponse> {
     return this.innerClient.validatorsUptime({
       validatorsUptimeRequest: {
         validator_addresses,
+        from_ledger_state,
+        at_ledger_state
       },
     })
   }
