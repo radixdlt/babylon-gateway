63d498e29baf07400bad952cd5e3d57746622d63
Paweł Pawelec <pawel.pawelec@rdx.works>
2024-04-23 15:50:04 +0200

    update node dependency to 1.1.3

---
+++ src/RadixDlt.CoreApiSdk/generated/Model/SignallingValidator.cs
@@ -0,0 +1,217 @@
+/* Copyright 2021 Radix Publishing Ltd incorporated in Jersey (Channel Islands).
+ *
+ * Licensed under the Radix License, Version 1.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at:
+ *
+ * radixfoundation.org/licenses/LICENSE-v1
+ *
+ * The Licensor hereby grants permission for the Canonical version of the Work to be
+ * published, distributed and used under or by reference to the Licensor’s trademark
+ * Radix ® and use of any unregistered trade names, logos or get-up.
+ *
+ * The Licensor provides the Work (and each Contributor provides its Contributions) on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied,
+ * including, without limitation, any warranties or conditions of TITLE, NON-INFRINGEMENT,
+ * MERCHANTABILITY, or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Whilst the Work is capable of being deployed, used and adopted (instantiated) to create
+ * a distributed ledger it is your responsibility to test and validate the code, together
+ * with all logic and performance of that code under all foreseeable scenarios.
+ *
+ * The Licensor does not make or purport to make and hereby excludes liability for all
+ * and any representation, warranty or undertaking in any form whatsoever, whether express
+ * or implied, to any entity or person, including any representation, warranty or
+ * undertaking, as to the functionality security use, value or other characteristics of
+ * any distributed ledger nor in respect the functioning or value of any tokens which may
+ * be created stored or transferred using the Work. The Licensor does not warrant that the
+ * Work or any use of the Work complies with any law or regulation in any territory where
+ * it may be implemented or used or that it will be appropriate for any specific purpose.
+ *
+ * Neither the licensor nor any current or former employees, officers, directors, partners,
+ * trustees, representatives, agents, advisors, contractors, or volunteers of the Licensor
+ * shall be liable for any direct or indirect, special, incidental, consequential or other
+ * losses of any kind, in tort, contract or otherwise (including but not limited to loss
+ * of revenue, income or profits, or loss of use or data, or loss of reputation, or loss
+ * of any economic or other opportunity of whatsoever nature or howsoever arising), arising
+ * out of or in connection with (without limitation of any use, misuse, of any ledger system
+ * or use made or its functionality or any performance or operation of any code or protocol
+ * caused by bugs or programming or logic errors or otherwise);
+ *
+ * A. any offer, purchase, holding, use, sale, exchange or transmission of any
+ * cryptographic keys, tokens or assets created, exchanged, stored or arising from any
+ * interaction with the Work;
+ *
+ * B. any failure in a transmission or loss of any token or assets keys or other digital
+ * artefacts due to errors in transmission;
+ *
+ * C. bugs, hacks, logic errors or faults in the Work or any communication;
+ *
+ * D. system software or apparatus including but not limited to losses caused by errors
+ * in holding or transmitting tokens by any third-party;
+ *
+ * E. breaches or failure of security including hacker attacks, loss or disclosure of
+ * password, loss of private key, unauthorised use or misuse of such passwords or keys;
+ *
+ * F. any losses including loss of anticipated savings or other benefits resulting from
+ * use of the Work or any changes to the Work (however implemented).
+ *
+ * You are solely responsible for; testing, validating and evaluation of all operation
+ * logic, functionality, security and appropriateness of using the Work for any commercial
+ * or non-commercial purpose and for any reproduction or redistribution by You of the
+ * Work. You assume all risks associated with Your use of the Work and the exercise of
+ * permissions under this License.
+ */
+
+/*
+ * Radix Core API - Babylon (Anemone)
+ *
+ * This API is exposed by the Babylon Radix node to give clients access to the Radix Engine, Mempool and State in the node.  The default configuration is intended for use by node-runners on a private network, and is not intended to be exposed publicly. Very heavy load may impact the node's function. The node exposes a configuration flag which allows disabling certain endpoints which may be problematic, but monitoring is advised. This configuration parameter is `api.core.flags.enable_unbounded_endpoints` / `RADIXDLT_CORE_API_FLAGS_ENABLE_UNBOUNDED_ENDPOINTS`.  This API exposes queries against the node's current state (see `/lts/state/` or `/state/`), and streams of transaction history (under `/lts/stream/` or `/stream`).  If you require queries against snapshots of historical ledger state, you may also wish to consider using the [Gateway API](https://docs-babylon.radixdlt.com/).  ## Integration and forward compatibility guarantees  Integrators (such as exchanges) are recommended to use the `/lts/` endpoints - they have been designed to be clear and simple for integrators wishing to create and monitor transactions involving fungible transfers to/from accounts.  All endpoints under `/lts/` have high guarantees of forward compatibility in future node versions. We may add new fields, but existing fields will not be changed. Assuming the integrating code uses a permissive JSON parser which ignores unknown fields, any additions will not affect existing code.  Other endpoints may be changed with new node versions carrying protocol-updates, although any breaking changes will be flagged clearly in the corresponding release notes.  All responses may have additional fields added, so clients are advised to use JSON parsers which ignore unknown fields on JSON objects. 
+ *
+ * The version of the OpenAPI document: v1.1.3
+ * Generated by: https://github.com/openapitools/openapi-generator.git
+ */
+
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using FileParameter = RadixDlt.CoreApiSdk.Client.FileParameter;
+using OpenAPIDateConverter = RadixDlt.CoreApiSdk.Client.OpenAPIDateConverter;
+
+namespace RadixDlt.CoreApiSdk.Model
+{
+    /// <summary>
+    /// SignallingValidator
+    /// </summary>
+    [DataContract(Name = "SignallingValidator")]
+    public partial class SignallingValidator : IEquatable<SignallingValidator>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SignallingValidator" /> class.
+        /// </summary>
+        [JsonConstructorAttribute]
+        protected SignallingValidator() { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SignallingValidator" /> class.
+        /// </summary>
+        /// <param name="index">index (required).</param>
+        /// <param name="activeStakeProportion">A proportion (between 0 and 1) of the total active stake of an entire &#x60;current_validator_set&#x60; (i.e. an easily-computable convenience field). This is a string-encoded fixed-precision decimal to 18 decimal places. A decimal is formed of some signed integer &#x60;m&#x60; of attos (&#x60;10^(-18)&#x60;) units, where &#x60;-2^(192 - 1) &lt;&#x3D; m &lt; 2^(192 - 1)&#x60;.  (required).</param>
+        public SignallingValidator(ActiveValidatorIndex index = default(ActiveValidatorIndex), string activeStakeProportion = default(string))
+        {
+            // to ensure "index" is required (not null)
+            if (index == null)
+            {
+                throw new ArgumentNullException("index is a required property for SignallingValidator and cannot be null");
+            }
+            this.Index = index;
+            // to ensure "activeStakeProportion" is required (not null)
+            if (activeStakeProportion == null)
+            {
+                throw new ArgumentNullException("activeStakeProportion is a required property for SignallingValidator and cannot be null");
+            }
+            this.ActiveStakeProportion = activeStakeProportion;
+        }
+
+        /// <summary>
+        /// Gets or Sets Index
+        /// </summary>
+        [DataMember(Name = "index", IsRequired = true, EmitDefaultValue = true)]
+        public ActiveValidatorIndex Index { get; set; }
+
+        /// <summary>
+        /// A proportion (between 0 and 1) of the total active stake of an entire &#x60;current_validator_set&#x60; (i.e. an easily-computable convenience field). This is a string-encoded fixed-precision decimal to 18 decimal places. A decimal is formed of some signed integer &#x60;m&#x60; of attos (&#x60;10^(-18)&#x60;) units, where &#x60;-2^(192 - 1) &lt;&#x3D; m &lt; 2^(192 - 1)&#x60;. 
+        /// </summary>
+        /// <value>A proportion (between 0 and 1) of the total active stake of an entire &#x60;current_validator_set&#x60; (i.e. an easily-computable convenience field). This is a string-encoded fixed-precision decimal to 18 decimal places. A decimal is formed of some signed integer &#x60;m&#x60; of attos (&#x60;10^(-18)&#x60;) units, where &#x60;-2^(192 - 1) &lt;&#x3D; m &lt; 2^(192 - 1)&#x60;. </value>
+        [DataMember(Name = "active_stake_proportion", IsRequired = true, EmitDefaultValue = true)]
+        public string ActiveStakeProportion { get; set; }
+
+        /// <summary>
+        /// Returns the string presentation of the object
+        /// </summary>
+        /// <returns>String presentation of the object</returns>
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("class SignallingValidator {\n");
+            sb.Append("  Index: ").Append(Index).Append("\n");
+            sb.Append("  ActiveStakeProportion: ").Append(ActiveStakeProportion).Append("\n");
+            sb.Append("}\n");
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Returns the JSON string presentation of the object
+        /// </summary>
+        /// <returns>JSON string presentation of the object</returns>
+        public virtual string ToJson()
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(this, Newtonsoft.Json.Formatting.Indented);
+        }
+
+        /// <summary>
+        /// Returns true if objects are equal
+        /// </summary>
+        /// <param name="input">Object to be compared</param>
+        /// <returns>Boolean</returns>
+        public override bool Equals(object input)
+        {
+            return this.Equals(input as SignallingValidator);
+        }
+
+        /// <summary>
+        /// Returns true if SignallingValidator instances are equal
+        /// </summary>
+        /// <param name="input">Instance of SignallingValidator to be compared</param>
+        /// <returns>Boolean</returns>
+        public bool Equals(SignallingValidator input)
+        {
+            if (input == null)
+            {
+                return false;
+            }
+            return 
+                (
+                    this.Index == input.Index ||
+                    (this.Index != null &&
+                    this.Index.Equals(input.Index))
+                ) && 
+                (
+                    this.ActiveStakeProportion == input.ActiveStakeProportion ||
+                    (this.ActiveStakeProportion != null &&
+                    this.ActiveStakeProportion.Equals(input.ActiveStakeProportion))
+                );
+        }
+
+        /// <summary>
+        /// Gets the hash code
+        /// </summary>
+        /// <returns>Hash code</returns>
+        public override int GetHashCode()
+        {
+            unchecked // Overflow is fine, just wrap
+            {
+                int hashCode = 41;
+                if (this.Index != null)
+                {
+                    hashCode = (hashCode * 59) + this.Index.GetHashCode();
+                }
+                if (this.ActiveStakeProportion != null)
+                {
+                    hashCode = (hashCode * 59) + this.ActiveStakeProportion.GetHashCode();
+                }
+                return hashCode;
+            }
+        }
+
+    }
+
+}
---
+++ src/RadixDlt.CoreApiSdk/generated/Model/StreamProofsRequest.cs
@@ -0,0 +1,246 @@
+/* Copyright 2021 Radix Publishing Ltd incorporated in Jersey (Channel Islands).
+ *
+ * Licensed under the Radix License, Version 1.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at:
+ *
+ * radixfoundation.org/licenses/LICENSE-v1
+ *
+ * The Licensor hereby grants permission for the Canonical version of the Work to be
+ * published, distributed and used under or by reference to the Licensor’s trademark
+ * Radix ® and use of any unregistered trade names, logos or get-up.
+ *
+ * The Licensor provides the Work (and each Contributor provides its Contributions) on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied,
+ * including, without limitation, any warranties or conditions of TITLE, NON-INFRINGEMENT,
+ * MERCHANTABILITY, or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Whilst the Work is capable of being deployed, used and adopted (instantiated) to create
+ * a distributed ledger it is your responsibility to test and validate the code, together
+ * with all logic and performance of that code under all foreseeable scenarios.
+ *
+ * The Licensor does not make or purport to make and hereby excludes liability for all
+ * and any representation, warranty or undertaking in any form whatsoever, whether express
+ * or implied, to any entity or person, including any representation, warranty or
+ * undertaking, as to the functionality security use, value or other characteristics of
+ * any distributed ledger nor in respect the functioning or value of any tokens which may
+ * be created stored or transferred using the Work. The Licensor does not warrant that the
+ * Work or any use of the Work complies with any law or regulation in any territory where
+ * it may be implemented or used or that it will be appropriate for any specific purpose.
+ *
+ * Neither the licensor nor any current or former employees, officers, directors, partners,
+ * trustees, representatives, agents, advisors, contractors, or volunteers of the Licensor
+ * shall be liable for any direct or indirect, special, incidental, consequential or other
+ * losses of any kind, in tort, contract or otherwise (including but not limited to loss
+ * of revenue, income or profits, or loss of use or data, or loss of reputation, or loss
+ * of any economic or other opportunity of whatsoever nature or howsoever arising), arising
+ * out of or in connection with (without limitation of any use, misuse, of any ledger system
+ * or use made or its functionality or any performance or operation of any code or protocol
+ * caused by bugs or programming or logic errors or otherwise);
+ *
+ * A. any offer, purchase, holding, use, sale, exchange or transmission of any
+ * cryptographic keys, tokens or assets created, exchanged, stored or arising from any
+ * interaction with the Work;
+ *
+ * B. any failure in a transmission or loss of any token or assets keys or other digital
+ * artefacts due to errors in transmission;
+ *
+ * C. bugs, hacks, logic errors or faults in the Work or any communication;
+ *
+ * D. system software or apparatus including but not limited to losses caused by errors
+ * in holding or transmitting tokens by any third-party;
+ *
+ * E. breaches or failure of security including hacker attacks, loss or disclosure of
+ * password, loss of private key, unauthorised use or misuse of such passwords or keys;
+ *
+ * F. any losses including loss of anticipated savings or other benefits resulting from
+ * use of the Work or any changes to the Work (however implemented).
+ *
+ * You are solely responsible for; testing, validating and evaluation of all operation
+ * logic, functionality, security and appropriateness of using the Work for any commercial
+ * or non-commercial purpose and for any reproduction or redistribution by You of the
+ * Work. You assume all risks associated with Your use of the Work and the exercise of
+ * permissions under this License.
+ */
+
+/*
+ * Radix Core API - Babylon (Anemone)
+ *
+ * This API is exposed by the Babylon Radix node to give clients access to the Radix Engine, Mempool and State in the node.  The default configuration is intended for use by node-runners on a private network, and is not intended to be exposed publicly. Very heavy load may impact the node's function. The node exposes a configuration flag which allows disabling certain endpoints which may be problematic, but monitoring is advised. This configuration parameter is `api.core.flags.enable_unbounded_endpoints` / `RADIXDLT_CORE_API_FLAGS_ENABLE_UNBOUNDED_ENDPOINTS`.  This API exposes queries against the node's current state (see `/lts/state/` or `/state/`), and streams of transaction history (under `/lts/stream/` or `/stream`).  If you require queries against snapshots of historical ledger state, you may also wish to consider using the [Gateway API](https://docs-babylon.radixdlt.com/).  ## Integration and forward compatibility guarantees  Integrators (such as exchanges) are recommended to use the `/lts/` endpoints - they have been designed to be clear and simple for integrators wishing to create and monitor transactions involving fungible transfers to/from accounts.  All endpoints under `/lts/` have high guarantees of forward compatibility in future node versions. We may add new fields, but existing fields will not be changed. Assuming the integrating code uses a permissive JSON parser which ignores unknown fields, any additions will not affect existing code.  Other endpoints may be changed with new node versions carrying protocol-updates, although any breaking changes will be flagged clearly in the corresponding release notes.  All responses may have additional fields added, so clients are advised to use JSON parsers which ignore unknown fields on JSON objects. 
+ *
+ * The version of the OpenAPI document: v1.1.3
+ * Generated by: https://github.com/openapitools/openapi-generator.git
+ */
+
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using FileParameter = RadixDlt.CoreApiSdk.Client.FileParameter;
+using OpenAPIDateConverter = RadixDlt.CoreApiSdk.Client.OpenAPIDateConverter;
+
+namespace RadixDlt.CoreApiSdk.Model
+{
+    /// <summary>
+    /// A request to retrieve a sublist of proofs. 
+    /// </summary>
+    [DataContract(Name = "StreamProofsRequest")]
+    public partial class StreamProofsRequest : IEquatable<StreamProofsRequest>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StreamProofsRequest" /> class.
+        /// </summary>
+        [JsonConstructorAttribute]
+        protected StreamProofsRequest() { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StreamProofsRequest" /> class.
+        /// </summary>
+        /// <param name="network">The logical name of the network (required).</param>
+        /// <param name="filter">filter.</param>
+        /// <param name="maxPageSize">If specified, the maximum number of proofs that will be returned..</param>
+        /// <param name="continuationToken">A continuation token is returned if and only if there are further non-empty pages of items currently available. The token can be provided in a following request to fetch the next page of results. The filter and sort should not be changed when re-using the continuation token. .</param>
+        public StreamProofsRequest(string network = default(string), StreamProofsFilter filter = default(StreamProofsFilter), int maxPageSize = default(int), string continuationToken = default(string))
+        {
+            // to ensure "network" is required (not null)
+            if (network == null)
+            {
+                throw new ArgumentNullException("network is a required property for StreamProofsRequest and cannot be null");
+            }
+            this.Network = network;
+            this.Filter = filter;
+            this.MaxPageSize = maxPageSize;
+            this.ContinuationToken = continuationToken;
+        }
+
+        /// <summary>
+        /// The logical name of the network
+        /// </summary>
+        /// <value>The logical name of the network</value>
+        [DataMember(Name = "network", IsRequired = true, EmitDefaultValue = true)]
+        public string Network { get; set; }
+
+        /// <summary>
+        /// Gets or Sets Filter
+        /// </summary>
+        [DataMember(Name = "filter", EmitDefaultValue = true)]
+        public StreamProofsFilter Filter { get; set; }
+
+        /// <summary>
+        /// If specified, the maximum number of proofs that will be returned.
+        /// </summary>
+        /// <value>If specified, the maximum number of proofs that will be returned.</value>
+        [DataMember(Name = "max_page_size", EmitDefaultValue = true)]
+        public int MaxPageSize { get; set; }
+
+        /// <summary>
+        /// A continuation token is returned if and only if there are further non-empty pages of items currently available. The token can be provided in a following request to fetch the next page of results. The filter and sort should not be changed when re-using the continuation token. 
+        /// </summary>
+        /// <value>A continuation token is returned if and only if there are further non-empty pages of items currently available. The token can be provided in a following request to fetch the next page of results. The filter and sort should not be changed when re-using the continuation token. </value>
+        [DataMember(Name = "continuation_token", EmitDefaultValue = true)]
+        public string ContinuationToken { get; set; }
+
+        /// <summary>
+        /// Returns the string presentation of the object
+        /// </summary>
+        /// <returns>String presentation of the object</returns>
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("class StreamProofsRequest {\n");
+            sb.Append("  Network: ").Append(Network).Append("\n");
+            sb.Append("  Filter: ").Append(Filter).Append("\n");
+            sb.Append("  MaxPageSize: ").Append(MaxPageSize).Append("\n");
+            sb.Append("  ContinuationToken: ").Append(ContinuationToken).Append("\n");
+            sb.Append("}\n");
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Returns the JSON string presentation of the object
+        /// </summary>
+        /// <returns>JSON string presentation of the object</returns>
+        public virtual string ToJson()
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(this, Newtonsoft.Json.Formatting.Indented);
+        }
+
+        /// <summary>
+        /// Returns true if objects are equal
+        /// </summary>
+        /// <param name="input">Object to be compared</param>
+        /// <returns>Boolean</returns>
+        public override bool Equals(object input)
+        {
+            return this.Equals(input as StreamProofsRequest);
+        }
+
+        /// <summary>
+        /// Returns true if StreamProofsRequest instances are equal
+        /// </summary>
+        /// <param name="input">Instance of StreamProofsRequest to be compared</param>
+        /// <returns>Boolean</returns>
+        public bool Equals(StreamProofsRequest input)
+        {
+            if (input == null)
+            {
+                return false;
+            }
+            return 
+                (
+                    this.Network == input.Network ||
+                    (this.Network != null &&
+                    this.Network.Equals(input.Network))
+                ) && 
+                (
+                    this.Filter == input.Filter ||
+                    (this.Filter != null &&
+                    this.Filter.Equals(input.Filter))
+                ) && 
+                (
+                    this.MaxPageSize == input.MaxPageSize ||
+                    this.MaxPageSize.Equals(input.MaxPageSize)
+                ) && 
+                (
+                    this.ContinuationToken == input.ContinuationToken ||
+                    (this.ContinuationToken != null &&
+                    this.ContinuationToken.Equals(input.ContinuationToken))
+                );
+        }
+
+        /// <summary>
+        /// Gets the hash code
+        /// </summary>
+        /// <returns>Hash code</returns>
+        public override int GetHashCode()
+        {
+            unchecked // Overflow is fine, just wrap
+            {
+                int hashCode = 41;
+                if (this.Network != null)
+                {
+                    hashCode = (hashCode * 59) + this.Network.GetHashCode();
+                }
+                if (this.Filter != null)
+                {
+                    hashCode = (hashCode * 59) + this.Filter.GetHashCode();
+                }
+                hashCode = (hashCode * 59) + this.MaxPageSize.GetHashCode();
+                if (this.ContinuationToken != null)
+                {
+                    hashCode = (hashCode * 59) + this.ContinuationToken.GetHashCode();
+                }
+                return hashCode;
+            }
+        }
+
+    }
+
+}
