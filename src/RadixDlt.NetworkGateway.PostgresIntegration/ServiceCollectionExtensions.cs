8353bf4dd332a8c77489d17d5726e4b58b2cb042
PawelPawelec-RDX <122867949+PawelPawelec-RDX@users.noreply.github.com>
2024-08-19 13:44:24 +0200

    allow configuring schema for database history table. (#785)
    * allow configuring schema for database history table

--- src/RadixDlt.NetworkGateway.PostgresIntegration/ServiceCollectionExtensions.cs
+++ src/RadixDlt.NetworkGateway.PostgresIntegration/ServiceCollectionExtensions.cs
@@ -63,6 +63,7 @@
  */
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
@@ -86,7 +87,25 @@ public static class ServiceCollectionExtensions
             {
                 options.UseNpgsql(
                     serviceProvider.GetRequiredService<NpgsqlDataSourceHolder<MigrationsDbContext>>().NpgsqlDataSource,
-                    o => o.MigrationsAssembly(typeof(MigrationsDbContext).Assembly.GetName().Name));
+                    o =>
+                    {
+                        o.MigrationsAssembly(typeof(MigrationsDbContext).Assembly.GetName().Name);
+
+                        // Now we work around https://github.com/npgsql/efcore.pg/issues/2878
+                        // "7.0.11 gives __EFMigrationsHistory already exists on dbContext.Database.Migrate();"
+                        // Which breaks migrations running against non-public default schemas
+                        var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+                        var connectionString = configuration.GetConnectionString(PostgresIntegrationConstants.Configuration.MigrationsConnectionStringName);
+                        var connectionStringBuilder = new NpgsqlConnectionStringBuilder(connectionString);
+                        var searchPaths = connectionStringBuilder.SearchPath?.Split(',');
+                        if (searchPaths is not { Length: > 0 })
+                        {
+                            return;
+                        }
+
+                        var overridenSchema = searchPaths[0];
+                        o.MigrationsHistoryTable(HistoryRepository.DefaultTableName, overridenSchema);
+                    });
             });
     }
 
